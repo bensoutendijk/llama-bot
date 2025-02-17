eecb77e1d6e049dd6d2a93a97f2f3696c07eaeb6
Ben Soutendijk <bensoutendijk@gmail.com>
2025-02-17 10:09:46 -0600

    minimize logging in prod

--- bot/tavern.py
+++ bot/tavern.py
@@ -11,21 +11,23 @@ from patron import Patron
 class Tavern(commands.Cog, name="tavern"):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        logger.info("Initializing Tavern cog...")
-        logger.info("Creating LLM client...")
+        logger.debug("Initializing Tavern cog...")
+        
+        logger.debug("Creating LLM client...")
+        
         self.client = OpenAI(
             base_url=f"{os.getenv('TAVERN_OPENAI_BASE')}/v1",
             api_key=os.getenv("TAVERN_OPENAI_API_KEY"),
         )
 
-        logger.info("Performing health check on LLM...")
-        res = self.check_health()
-        logger.info(f"Health check result: {res}")
+        logger.debug("Performing health check on LLM...")
+        self.check_health()
+        
         self.patron = Patron("template", self.bot, self.client)
 
     def check_health(self):
         try:
-            logger.info(f"Connecting to url: {os.getenv('TAVERN_OPENAI_BASE')}/health")
+            logger.debug(f"Connecting to url: {os.getenv('TAVERN_OPENAI_BASE')}/health")
             response = requests.get(f"{os.getenv('TAVERN_OPENAI_BASE')}/health")
             response.raise_for_status()  # Raises an error for bad status codes
             return response.status_code == 200
@@ -35,10 +37,9 @@ class Tavern(commands.Cog, name="tavern"):
     
     @commands.Cog.listener()
     async def on_message(self, message: Message) -> None:
-        logger.info(f"Received message: {message.content}")
+        logger.debug(f"Received message: {message.content}")
         await self.patron.hear(message)
 
 async def setup(bot: commands.Bot) -> None:
-    logger.info("Setting up Tavern cog...")
+    logger.debug("Setting up Tavern cog...")
     await bot.add_cog(Tavern(bot))
-    logger.info("Tavern cog setup complete.")
