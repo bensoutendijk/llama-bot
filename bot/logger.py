63572505cbfb3aa2eb8e3ff3d584ea5b4e196edf
Ben Soutendijk <bensoutendijk@gmail.com>
2025-02-17 10:07:57 -0600

    add LOG_LEVEL environment variable

--- bot/logger.py
+++ bot/logger.py
@@ -1,19 +1,22 @@
 import logging
+import os
 import sys
 
 
 def setup_logger():
     logger = logging.getLogger('tavern_bot')
-    logger.setLevel(logging.INFO)
-    
+
+    log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
+    logger.setLevel(getattr(logging, log_level))
+
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
-    
+    handler.setLevel(getattr(logging, log_level))
+
     formatter = logging.Formatter('%(asctime)s %(levelname)s\t%(name)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     handler.setFormatter(formatter)
-    
+
     logger.addHandler(handler)
-    
+
     return logger
 
 logger = setup_logger()
